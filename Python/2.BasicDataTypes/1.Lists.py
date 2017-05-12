ffe248c49243d7d85b68c64fee9faa658e0ac544
wrightkim <wdk7bj@virginia.edu>
2017-05-12 17:43:24 -0400

    List

--- Python/2.BasicDataTypes/1.Lists.py
+++ Python/2.BasicDataTypes/1.Lists.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
         cmd = s[0]
         arg = s[1:]
         if cmd != 'print':
+            # ex) cmd = insert(0,5)
             cmd += "(" + ",".join(arg) + ")"
+            #eval l.insert(0,5)
             eval("l."+cmd)
         else:
             print(l)

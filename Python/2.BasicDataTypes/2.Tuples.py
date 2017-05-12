08b0ca0537db9710e5647adb7e1b5212ef4be1ae
wrightkim <wdk7bj@virginia.edu>
2017-05-12 17:50:51 -0400

    Tuple

--- Python/2.BasicDataTypes/2.Tuples.py
+++ Python/2.BasicDataTypes/2.Tuples.py
@@ -1,7 +1,9 @@
 #!/user/bin/env python3
 if __name__ == '__main__':
     n = int(input())
+    # [*map(func, item)] stores value as a list, integer_list
     integer_list = [*map(int, input().split())]
+    # converts list to tuple
     t= tuple(integer_list)
     print(hash(t))
 
